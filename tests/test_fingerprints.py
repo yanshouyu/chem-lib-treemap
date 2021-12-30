062575834f12def8f0674e1db6267672805b9039
Shouyu Yan <yan.shouyu@foxmail.com>
2021-12-30 22:17:06 +0800

    fix importing bug in test

--- tests/test_fingerprints.py
+++ tests/test_fingerprints.py
@@ -1,7 +1,7 @@
 import pytest
-import chemlibtreemap
+from chemlibtreemap import fingerprints
 
 def test_fp_list():
-    fp_list = chemlibtreemap.get_available_fp_generators()
+    fp_list = fingerprints.get_available_fp_generators()
     assert len(fp_list) > 0
 
--- tests/test_mol_descriptors.py
+++ tests/test_mol_descriptors.py
@@ -1,6 +1,6 @@
 import pytest
-import chemlibtreemap
+from chemlibtreemap import mol_descriptors
 
 def test_descriptor_list():
-    desc_list = chemlibtreemap.get_available_desc_generators()
+    desc_list = mol_descriptors.get_available_desc_generators()
     assert len(desc_list) > 0
